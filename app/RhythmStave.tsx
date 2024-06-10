e22d436beedd469c2eb95738e1f1afa3a7bfd34a
adamsaunders26 <a.saunders206@gmail.com>
2024-06-10 11:00:51 +0100

    start to create rhythm layout

--- app/RhythmStave.tsx
+++ app/RhythmStave.tsx
@@ -1,3 +1,18 @@
+import RhythmNumbers from "./RhythmNumbers";
+
 export default function RhythmStave() {
-  return <div>I'm a ryhtm man</div>;
+  return (
+    <div className="flex">
+      <div className="border-2 border-orange-500">
+        <div>Beats</div>
+        <div>Keys</div>
+        <div>Guitar</div>
+      </div>
+      <div className="border-2 border-blue-500 flex-1">
+        <RhythmNumbers />
+        <div>crotchets</div>
+        <div>crotchets</div>
+      </div>
+    </div>
+  );
 }
