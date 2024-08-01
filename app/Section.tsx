12fd2c8e80da9533a13d9f90f8fdc7e083edc9a8
Adam Saunders <adamsaunders@adams-mbp.home>
2024-08-01 16:59:50 +0100

    new sections can be added to localStorage

--- app/Section.tsx
+++ app/Section.tsx
@@ -5,11 +5,16 @@ import SectionNameInput from "./SectionNameInput";
 import DrumsUnit from "./Drums Unit/DrumUnit";
 import KeysUnit from "./Keys Unit/KeysUnit";
 import { useContext } from "react";
-import { iRockContext, iRockContextType } from "./Context";
+import { iRockContext, iRockContextType, iRockDataType } from "./Context";
 
-export default function Section() {
+export default function Section({
+  sectionData,
+}: {
+  sectionData: iRockDataType;
+}) {
   // const { section, tools } = useContext<iRockContextType>(iRockContext);
   // const currentSong = tools.currentSong[0]
+  console.log(sectionData);
   return (
     <div className="border-2 border-purple-500 flex flex-col justify-end w-full">
       <SectionNameInput />
--- app/Song.tsx
+++ app/Song.tsx
@@ -1,16 +1,66 @@
 "use client";
 
-import { useContext } from "react";
-import { iRockContext, iRockContextType } from "./Context";
+import { useContext, useState } from "react";
+import { iRockContext, iRockContextType, iRockDataType } from "./Context";
+import { blankSectionStruct } from "./modelData";
+import Section from "./Section";
 
 export default function Song() {
   const { section, tools } = useContext<iRockContextType>(iRockContext);
   const currentSong = tools.currentSong[0];
+  const [songList, setSongList] = tools.songList;
+  const [currentSections, setCurrentSections] = useState(null);
+  const [currentSectionNumber, setCurrentSectionNumber] = useState(null);
+
+  let currentSongData: unknown;
+  if (currentSong) {
+    currentSongData = songList.songList[currentSong];
+  }
+
+  const currSects = Object.keys(songList.songList[currentSong]);
+  console.log(currSects);
   return (
     <div className="flex flex-col">
       <div>Current Song: {currentSong}</div>
-      <button>New Section</button>
-      Here are all the songs!
+      <button
+        className="bg-pink-400 mx-auto rounded-lg m-1 p-1"
+        onClick={() => {
+          const songListCopy = JSON.parse(JSON.stringify(songList));
+          if (Object.keys(currentSongData).length === 0) {
+            songListCopy.songList[currentSong][1] = blankSectionStruct;
+            localStorage.setItem("songList", JSON.stringify(songListCopy));
+            setSongList(songListCopy);
+          } else {
+            const currentNumber =
+              Object.keys(currentSongData)[
+                Object.keys(currentSongData).length - 1
+              ];
+
+            songListCopy.songList[currentSong][Number(currentNumber) + 1] =
+              blankSectionStruct;
+            localStorage.setItem("songList", JSON.stringify(songListCopy));
+            setSongList(songListCopy);
+          }
+        }}
+      >
+        New Section
+      </button>
+      <button
+        className="bg-orange-400 mx-auto rounded-lg m-1 p-1"
+        onClick={() => {
+          console.log(JSON.parse(localStorage.getItem("songList")));
+        }}
+      >
+        Check storage
+      </button>
+      {currSects.map((value, i) => {
+        return (
+          <Section
+            key={i}
+            sectionData={songList.songList[currentSong][value]}
+          />
+        );
+      })}
     </div>
   );
 }
